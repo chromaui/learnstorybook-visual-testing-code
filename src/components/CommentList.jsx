3bf98f72fa4a0a42b517e69205f3255b9aa9677f
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-04-26 21:01:40 +0100

    CommentList Initial State

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,45 @@
+import PropTypes from "prop-types";
+
+export default function CommentList({ loading, comments, totalCount }) {
+  if (loading) {
+    return <div>loading</div>;
+  }
+  if (comments.length === 0) {
+    return <div>empty</div>;
+  }
+  return (
+    <div>
+      {comments.length} of {totalCount}
+    </div>
+  );
+}
+
+CommentList.propTypes = {
+  /**
+   * Is the component in the loading state
+   */
+  loading: PropTypes.bool,
+
+  /**
+   * Total number of comments
+   */
+  totalCount: PropTypes.number,
+  /**
+   * List of comments
+   */
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string,
+      author: PropTypes.shape({
+        name: PropTypes.string,
+        avatar: PropTypes.string,
+      }),
+    })
+  ),
+};
+
+CommentList.defaultProps = {
+  loading: false,
+  totalCount: 10,
+  comments: [],
+};
