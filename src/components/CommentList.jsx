ba7561e55ad8fc86d6cd2ff8f0749ab8cfc7fa79
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-11-02 21:33:02 +0000

    CommentList updated for "Has Data" use case

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,5 +1,67 @@
 import PropTypes from "prop-types";
 
+import styled, { createGlobalStyle } from "styled-components";
+
+const CommentListDiv = styled.div`
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: #333;
+  display: inline-block;
+  vertical-align: top;
+  width: 265px;
+`;
+
+const CommentItemDiv = styled.div`
+  font-size: 12px;
+  line-height: 14px;
+  clear: both;
+  height: 48px;
+  margin-bottom: 10px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 0 10px 0;
+  background: linear-gradient(
+    120deg,
+    rgba(248, 248, 254, 0.95),
+    rgba(250, 250, 250, 0.95)
+  );
+  border-radius: 48px;
+`;
+
+const AvatarDiv = styled.div`
+  float: left;
+  position: relative;
+  overflow: hidden;
+  height: 48px;
+  width: 48px;
+  margin-right: 14px;
+  background: #dfecf2;
+  border-radius: 48px;
+`;
+
+const AvatarImg = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  background: #999;
+`;
+
+const MessageDiv = styled.div`
+  overflow: hidden;
+  padding-top: 10px;
+  padding-right: 20px;
+`;
+
+const AuthorSpan = styled.span`
+  font-weight: bold;
+`;
+
+const TextSpan = styled.span``;
+
+const GlobalStyle = createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,800');
+ `;
+
 export default function CommentList({ loading, comments, totalCount }) {
   if (loading) {
     return <div>loading</div>;
@@ -8,9 +70,21 @@ export default function CommentList({ loading, comments, totalCount }) {
     return <div>empty</div>;
   }
   return (
-    <div>
-      {comments.length} of {totalCount}
-    </div>
+    <>
+      <GlobalStyle />
+      <CommentListDiv>
+        {comments.map(({ text, author: { name, avatar } }) => (
+          <CommentItemDiv key={`comment_${name}`}>
+            <AvatarDiv>
+              <AvatarImg src={avatar} />
+            </AvatarDiv>
+            <MessageDiv>
+              <AuthorSpan>{name}</AuthorSpan> <TextSpan>{text}</TextSpan>
+            </MessageDiv>
+          </CommentItemDiv>
+        ))}
+      </CommentListDiv>
+    </>
   );
 }
 
